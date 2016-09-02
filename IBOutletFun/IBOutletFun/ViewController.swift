57a82004f50d38cf171fe0fb85e318acd812198c
Tameika Lawrence <tameikalawrence@Tameikas-MacBook-Air.local>
2016-09-02 15:16:59 -0400

    hello and goodbye app

--- IBOutletFun/IBOutletFun/ViewController.swift
+++ IBOutletFun/IBOutletFun/ViewController.swift
@@ -9,14 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var textDisplay: UILabel!
 
+    @IBAction func helloWorld(sender: AnyObject) {
+        
+    textDisplay.text = "Hello, world!"
+        print("Pressed Hello World button")
+    }
+    
+    @IBAction func goodbyeWorld(sender: AnyObject) {
+        textDisplay.text = "Goodbye, world!"
+        print("Pressed Goodbye World button")
+    }
+    
+    @IBAction func unicorn(sender: AnyObject) {
+        textDisplay.text = "x"
+        print("Pressed Unicorn button")
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        textDisplay.text = "Zzz..."
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+        
         // Dispose of any resources that can be recreated.
     }
 }
